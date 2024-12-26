159d0d9cb2a7e44eed05b639febcceb0dec76cba
TaniMystic <88611927+from-tanim@users.noreply.github.com>
2024-12-26 08:51:39 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,22 @@
-const CACHE_NAME = 'number-system-converter-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/mainJs.js',
-  '/manifest.json',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
-];
-
+// Install event - caching important files
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+    caches.open('number-system-converter-v1').then((cache) => {
+      return cache.addAll([
+        '/',               // root page
+        '/index.html',     // HTML page
+        '/style.css',      // CSS stylesheet
+        '/mainJs.js'       // JavaScript file
+      ]);
     })
   );
 });
 
+// Fetch event - serving cached files when offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
+      // If there's a cached match, return it, otherwise fetch from network
       return response || fetch(event.request);
     })
   );
